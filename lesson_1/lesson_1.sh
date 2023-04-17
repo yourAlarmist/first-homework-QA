29612b337bd01759eaed83c05beced50ffda495c
Viktar Buzyuk <pdv5616342@gmail.com>
2023-04-17 22:03:56 +0300

    first homework

---
+++ lesson_1/lesson_1.sh
@@ -0,0 +1,8 @@
+cd folder_1
+mkdir folder-1_1 folder-1_2 folder-1_3
+cd folder-1_2
+touch newfile1.txt newfile2.txt newfile3.txt newfile4.json newfile5.json
+mkdir newfolder1 newfolder2 newfolder3
+ls
+mv ./newfile1.txt ./newfolder1
+mv ./newfile4.json ./newfolder2
